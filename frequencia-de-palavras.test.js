e36b57ca1a3ba67f9faa50390b42baec895f5902
MilenaRichesky <milenabornoldo1515@gmail.com>
2022-08-16 22:11:43 -0300

    Melhoria no texte unitario

--- frequencia-de-palavras.test.js
+++ frequencia-de-palavras.test.js
@@ -1,20 +1,29 @@
 var frequenciaDePalavras = require('./index');
 
+const theory = [
+  {
+    word: "crescer",
+    phrase: "Quero fazer o crescer para poder crescer profissionalmente",
+    result: "A palavra crescer aparece 2 de vezes na frase"
+  },
+  {
+    word: "amar",
+    phrase: "Amar e sonhar, sonhar e viver, viver e curtir, curtir e amar",
+    result: "A palavra amar aparece 2 de vezes na frase"
+  },
+  {
+    word: "carro",
+    phrase: "Adoro esportes",
+    result: "A palavra carro aparece 0 de vezes na frase"
+  },
+]
+
 describe('Frequencia de palavras', () => {
+  let value
 
-  test('Deve testar a frase "Quero fazer o crescer para poder crescer profissionalmente"', () => {
-    var resultado = frequenciaDePalavras("crescer", "Quero fazer o crescer para poder crescer profissionalmente")
-    expect(resultado).toEqual("A palavra crescer aparece 2 de vezes na frase");
-  })
-
-  test('Deve testar a frase "Amar e sonhar, sonhar e viver, viver e curtir, curtir e amar"', () => {
-    var resultado = frequenciaDePalavras("amar", "Amar e sonhar, sonhar e viver, viver e curtir, curtir e amar")
-    expect(resultado).toEqual("A palavra amar aparece 2 de vezes na frase");
-  })
-
-  test('Deve testar uma frase sem ocorrencias', () => {
-    var resultado = frequenciaDePalavras("carro", "Adoro esportes")
-    expect(resultado).toEqual("A palavra carro aparece 0 de vezes na frase");
-  })
+  test.each(theory)('Deve testar a frase "$phrase"', ({ word, phrase, result }) => {
+    value = frequenciaDePalavras(word, phrase)
+    expect(value).toEqual(result);
+  });
 
 });
